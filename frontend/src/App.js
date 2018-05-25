89524e37288aa23299afb6a2ea839c8ec079958a
Carter <carterklein13@gmail.com>
2018-05-24 23:45:59 -0400

    Added Alphavantage API with functionality when user clicks on one of the stocks in his portfolio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -130,22 +130,37 @@ class Stock extends React.Component{
       name: '',
       count: null,
       first_bought_date: '',
+      open_price_today: null,
+      high_price_today: null,
+      low_price_today: null,
+      close_price_today: null,
+      total_value_today: null, //how much you own (count * close for now)
     }
   }
   componentDidMount(){
     const { match : { params } } = this.props;
+    const alpha = require('alphavantage')({ key: 'B5VDVRB32WH3LOLR' });
     console.log("Stock id: " + params.stockId);
-    axios.get(`http://127.0.0.1:8000/api/stocks/${params.stockId}`)
-    .then(function(res){
-      console.log(res.data);
-    });
     axios.get(`http://127.0.0.1:8000/api/stocks/${params.stockId}`)
     .then(({ data: stock }) => {
       this.setState({
         owner: stock.owner,
-        name: stock.name,
+        name: stock.name.toUpperCase(),
         count: stock.count,
         first_bought_date: stock.first_bought_date,
+      });
+      alpha.data.daily(this.state.name)
+      .then(data=>{
+        // console.log(data["Time Series (Daily)"]["2018-05-22"]["1. open"]);
+        // console.log(data["Time Series (Daily)"]["2018-05-22"]["2. high"]);
+        console.log(parseFloat(data["Time Series (Daily)"]["2018-05-22"]["4. close"]).toFixed(4));
+        this.setState({
+          open_price_today: data["Time Series (Daily)"]["2018-05-22"]["1. open"],
+          high_price_today: data["Time Series (Daily)"]["2018-05-22"]["2. high"],
+          low_price_today: data["Time Series (Daily)"]["2018-05-22"]["3. low"],
+          close_price_today: data["Time Series (Daily)"]["2018-05-22"]["4. close"],
+          total_value_today: parseFloat(data["Time Series (Daily)"]["2018-05-22"]["4. close"] * this.state.count).toFixed(4),
+        });
       });
     })
     .catch(function(err){
@@ -155,11 +170,13 @@ class Stock extends React.Component{
   render(){
     return(
       <div>
-        <h1>Stock Detail Page for {this.state.name} stock of {this.state.owner}</h1>
-        <h4>User: {this.state.owner}</h4>
-        <h5>Name: {this.state.name}</h5>
-        <h5>Count: {this.state.count}</h5>
-        <h5>First Bought On: {this.state.first_bought_date}</h5>
+        <h1>Stock Detail Page for {this.state.name} stock of User {this.state.owner}</h1>
+        <h5>You Own {this.state.count} shares of {this.state.name}</h5>
+        <h5>{this.state.name} Open Price Today: {this.state.open_price_today}</h5>
+        <h5>{this.state.name} High Price Today: {this.state.high_price_today}</h5>
+        <h5>{this.state.name} Low Price Today: {this.state.low_price_today}</h5>
+        <h5>{this.state.name} Close Price Today: {this.state.close_price_today}</h5>
+        <h5>Your Current Investment in {this.state.name}: {this.state.total_value_today}</h5>
       </div>
     );
   }
