976eb575b88a31353117c58c869fe640acd7bc4a
Carter <carterklein13@gmail.com>
2018-06-19 07:25:26 -0400

    Got everything set so that frontend can run on port 8000

---
+++ backend/stock_app_api/frontend/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+
+export default class Home extends React.Component {
+  render() {
+  return (
+    <div>
+    <h2>Welcome to Stock App!</h2>
+    <p>
+      Let's get started making this correctly this time :)
+    </p>
+    </div>
+  )
+  }
+}
