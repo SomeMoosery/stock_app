83dfd3f05950899119612f9fd64f5ca60b459ec8
Carter <carterklein13@gmail.com>
2018-06-21 22:54:01 -0400

    Removed old frontend folder, we don't need 'er anymore :)

--- frontend/src/actions/index.js
+++
@@ -1,4 +0,0 @@
-import * as stocks from './stocks';
-import * as auth from './auth';
-
-export {stocks, auth}
--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import stocks from './stocks';
-import auth from './auth';
-
-const stockApp = combineReducers({
-  stocks, auth,
-})
-
-export default stockApp;
