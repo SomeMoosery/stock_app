54e7ee953bf0d59cca6a8fdd2a75958a6c28d597
Carter <carterklein13@gmail.com>
2018-07-13 23:05:43 -0400

    Changed boring initial loading... message to current nice loading

--- backend/stock_app_api/frontend/src/App.js
+++ backend/stock_app_api/frontend/src/App.js
@@ -30,7 +30,22 @@ class RootContainerComponent extends Component {
   PrivateRoute = ({component: ChildComponent, ...rest}) => {
     return <Route {...rest} render={props => {
       if (this.props.auth.isLoading) {
-        return <em>Loading...</em>;
+        return (
+          <div style={{textAlign:'center'}}>
+                    <div className="sk-cube-grid">
+                        <div className="sk-cube sk-cube1"></div>
+                        <div className="sk-cube sk-cube2"></div>
+                        <div className="sk-cube sk-cube3"></div>
+                        <div className="sk-cube sk-cube4"></div>
+                        <div className="sk-cube sk-cube5"></div>
+                        <div className="sk-cube sk-cube6"></div>
+                        <div className="sk-cube sk-cube7"></div>
+                        <div className="sk-cube sk-cube8"></div>
+                        <div className="sk-cube sk-cube9"></div>
+                    </div>
+                    <p>Welcome</p>
+                </div>
+        )
       } else if (!this.props.auth.isAuthenticated) {
         return <Redirect to="/login" />;
       } else {
