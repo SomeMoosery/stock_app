80995945e776ff9b7be49814d6ce6ab37c5e2ffb
Carter <carterklein13@gmail.com>
2018-07-30 21:28:39 -0400

    Updated pricing

--- backend/stock_app_api/frontend/src/components/Pricing.jsx
+++ backend/stock_app_api/frontend/src/components/Pricing.jsx
@@ -45,7 +45,7 @@ class Pricing extends React.Component{
                             EARN MONEY SECURELY
                           </Typography><hr style={{width:'60%'}}/>
                           <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
-                            One of two core goals of Unloan is to provide you, the savvy investor, with a 
+                            One of two core goals of Reciprocal is to provide you, the savvy investor, with a 
                             means to make sound investments with higher-than-average returns. No longer will
                             you have to use your hard-earned funds betting on those call options. 
                             No longer will you have to hope this new biotech will be the one to take off.
@@ -85,9 +85,41 @@ class Pricing extends React.Component{
                             PRICING
                           </Typography><hr style={{width:'60%'}}/>
                           <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
-                            Below, you'll find the pricing model we found worked best here at Unloan. 
+                            Below, you'll find the pricing model we found worked best here at Reciprocal. 
                             We've crunched a lot of numbers to ensure a perfect medium to ensure that 
                             whether you're giving a loan or receiving a loan, you're happy. Take a look:
+                          </Typography>
+
+                          <Typography style={{marginBottom:'1em', fontSize:'24px', textAlign:'center'}}>
+                            AS A PASSIVE INVESTMENT OPPORTUNITY
+                          </Typography><hr style={{width:'60%'}}/>
+                          <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
+                            You deposit the amount you want to invest
+                          </Typography>
+                          <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
+                            We use our well-trained team of algorithms to invest your funds in providing loans as efficiently as possible, 
+                            in order to earn you the largest returns possible. 
+                          </Typography>
+                          <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
+                            You reap the rewards of each individual transaction.
+                          </Typography>
+                          <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
+                            While you will see your returns as a lump sum total, each individual loan you give out will be priced
+                            according to the metrics for individual loans, displayed below.
+                          </Typography>
+
+                          <Typography style={{marginBottom:'1em', fontSize:'24px', textAlign:'center'}}>
+                            AS AN ACTIVE INVESTMENT OPPORTUNITY
+                          </Typography><hr style={{width:'60%'}}/>
+                          <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
+                            All loans under $200 will be charged a flat initial fee of $10.
+                          </Typography>
+                          <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
+                            For loans over $200, a small fee in the form of a slice of the interest revenue will be taken off of each loan.
+                          </Typography><br/><hr style={{width:'60%'}}/><br/>
+                          <Typography style={{marginBottom:'0.5em', fontSize:'16px'}}>
+                            We seek to provide you will full transparency about what we do here at Reciprocal Financial. If you have any questions
+                            or concerns, please feel free to reach out and we'd be happy to get in touch. 
                           </Typography>
                       </CardContent>
                     </Card>
