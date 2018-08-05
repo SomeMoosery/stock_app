ec797b771e00c2a5bdedcdaed92f75bafea64b6d
Carter <carterklein13@gmail.com>
2018-08-05 13:46:34 -0700

    Fixed up Add Offer component

--- backend/stock_app_api/frontend/src/components/AddOffer.jsx
+++ backend/stock_app_api/frontend/src/components/AddOffer.jsx
@@ -6,7 +6,29 @@ import {Link, Redirect} from 'react-router-dom';
 
 import {offer} from '../actions';
 
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+
+import { withStyles } from '@material-ui/core/styles';
+import { compose } from 'redux';
+
+import AppBar from './AppBar';
+
+
+const styles = theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit,
+      width: 200,
+    },
+    menu: {
+      width: 200,
+    },
+  });
 
 class AddOffer extends React.Component{
     state = {
@@ -35,25 +57,78 @@ class AddOffer extends React.Component{
     }
 
     render(){
+        const {classes} = this.props;
+
         if (this.state.submitted){
             return <Redirect to='/profile'/>
         }
 
         return(
             <div>
-                <Link to='/profile' style={{textDecoration:'none', color:'red'}}>
-                    <Button color='secondary'>
-                        <p>Back</p>
-                    </Button>
-                </Link>
-                <form onSubmit={this.submitOffer}>
-                    <input value={this.state.offerTitle} placeholder="Enter offer here" onChange={(e) => this.setState({offerTitle: e.target.value})} style = {{width: '80%'}} required /><br/>
-                    <input value={this.state.offerDescription} placeholder="Add a description" onChange={(e) => this.setState({offerDescription: e.target.value})} style = {{width: '80%'}} required /><br/>                            
-                    <input value={this.state.offerAmount} placeholder="How much are you willing to loan?" onChange={(e) => this.setState({offerAmount: e.target.value})} style = {{width: '80%'}} required/><br/>
-                    <input value={this.state.offerNumWeeks} placeholder="Over how many weeks will you expect a settlement?" onChange={(e) => this.setState({offerNumWeeks: e.target.value})} style = {{width: '80%'}} required/><br/>
-                    <input value={this.state.offerInterest} placeholder="What interest do you want to charge?" onChange={(e) => this.setState({offerInterest: e.target.value})} style = {{width: '80%'}} required/><br/>
+                <AppBar/>
+                <div style={{height:'6em'}}></div>
+                <form onSubmit={this.submitOffer} style={{textAlign:'center', marginLeft:'1em'}}>
+                    <TextField
+                        required
+                        style={{width:'30%'}}
+                        value={this.state.offerTitle}
+                        id="offerTitle"
+                        label="Offer Title"
+                        placeholder="Enter offer here"
+                        className={classes.textField}
+                        margin="normal"
+                        htmlFor="offerTitle"
+                        type="text"
+                        onChange={e => this.setState({offerTitle: e.target.value})}
+                    /><br/>
+                    <TextField
+                        required
+                        style={{width:'60%'}}
+                        value={this.state.offerDescription}
+                        id="offerDescription"
+                        label="Offer Description"
+                        placeholder="Tell us about your offer"
+                        className={classes.textField}
+                        margin="normal"
+                        htmlFor="offerDescription"
+                        type="text"
+                        onChange={e => this.setState({offerDescription: e.target.value})}
+                    /><br/>
+                    <TextField
+                        id="offerAmount"
+                        style={{width:'30%'}}
+                        label="Offer Amount"
+                        placeholder="How much are you willing to loan?"
+                        className={classes.textField}
+                        margin="normal"
+                        htmlFor="offerAmount"
+                        type="text"
+                        onChange={e => this.setState({offerAmount: e.target.value})}
+                    /><br/>
+                    <TextField
+                        id="offerNumWeeks"
+                        style={{width:'30%'}}
+                        label="Offer Number of Weeks"
+                        placeholder="Over how many weeks will you expect repayment?"
+                        className={classes.textField}
+                        margin="normal"
+                        htmlFor="offerNumWeeks"
+                        type="text"
+                        onChange={e => this.setState({offerNumWeeks: e.target.value})}
+                    /><br/>
+                    <TextField
+                        id="offerInterest"
+                        style={{width:'30%'}}
+                        label="Offer Interest"
+                        placeholder="How much interest do you want to charge?"
+                        className={classes.textField}
+                        margin="normal"
+                        htmlFor="offerInterest"
+                        type="text"
+                        onChange={e => this.setState({offerInterest: e.target.value})}
+                    /><br/>
+                    <div style={{height:'3em'}}></div>
                     <Button type="submit" color="primary" variant='outlined'>Post an Offer</Button><br/>
-                    <Button onClick={this.resetOfferForm} color="secondary" variant='outlined'>Reset</Button>
                 </form>
             </div>
         )
@@ -74,4 +149,7 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddOffer);
+export default compose(
+    withStyles(styles),
+    connect(mapStateToProps, mapDispatchToProps)
+  )(AddOffer);
