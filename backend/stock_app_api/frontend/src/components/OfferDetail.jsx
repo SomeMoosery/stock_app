07eddd812b29404795d90d9f8fd0ac630b680955
Carter <carterklein13@gmail.com>
2018-08-14 00:27:47 -0400

    Fixing up offer detail

--- backend/stock_app_api/frontend/src/components/OfferDetail.jsx
+++ backend/stock_app_api/frontend/src/components/OfferDetail.jsx
@@ -6,6 +6,8 @@ import {Link} from 'react-router-dom';
 
 import {offer, auth} from '../actions';
 
+import AppBar from './AppBar';
+
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import Dialog from '@material-ui/core/Dialog';
@@ -74,6 +76,8 @@ class OfferDetail extends React.Component{
 
         return(
             <div>
+                <AppBar/>
+                <div style={{height:'6em'}}></div>
                 <table>
                     <tbody>
                         {this.props.offers.map((offer, id) => (
